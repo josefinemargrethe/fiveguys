c56cd736a029490c006ed9d8aa51a0d4e9bfcf34
Josefine Margrethe Smidt Hansen <josefinemargrethe@Josefines-MacBook-Pro-2.local>
2018-12-13 14:07:44 +0100

    five guys fronter

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "80%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+function goBack() {
+    window.history.back()
+}
